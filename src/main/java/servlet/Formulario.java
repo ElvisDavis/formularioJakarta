a727e561fcac51951072701a686d25599f1f6ff0
Elvis <davis-elvis@hotmail.com>
2024-11-11 19:26:24 -0500

    formulario obtniendo parametros 2

--- src/main/java/servlet/Formulario.java
+++ src/main/java/servlet/Formulario.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 @WebServlet("/registro")
 public class Formulario extends HttpServlet {
@@ -18,6 +19,13 @@ public class Formulario extends HttpServlet {
         String username=req.getParameter("username");
         String password = req.getParameter("password");
         String email = req.getParameter("email");
+        //Segunda parte del formulario
+        String pais=req.getParameter("pais");
+        String[] lenguajes = req.getParameterValues("lenguajes");
+        String[] roles = req.getParameterValues("roles");
+        String idioma=req.getParameter("idioma");
+        boolean habilitar=req.getParameter("habilitar") != null && req.getParameter("habilitar").equals("on");
+
         PrintWriter out = resp.getWriter();
         //Creo la plantilla html
         out.print("<!DOCTYPE html>");
@@ -33,6 +41,17 @@ public class Formulario extends HttpServlet {
         out.println("<p>User: " + username + "</p>");
         out.println("<p>Password: " + password + "</p>");
         out.println("<p>Email: " + email + "</p>");
+        out.println("<p>Pais: " + pais + "</p>");
+        out.println("<h2>Lenguajes</h2>");
+        Arrays.asList(lenguajes).forEach(lenguaje -> {
+            out.println("<p>" + lenguaje + "</p>");
+        });
+
+        out.println("<p> habilitar "+ habilitar + "</p>");
+
+        //Vamos añadir un boton para regresar
+        out.println("<p><a href=\"/formulario/index.html\">Volver</a></p>");
+
         out.println("</div>");
         out.println("</body>");
         out.println("</html>");
